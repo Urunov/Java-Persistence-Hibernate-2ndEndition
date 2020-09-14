5084c81cce03f01e3c6cd97147e69318c65c07d1
apple <hamdamboy.urunov@gmail.com>
2020-09-14 11:56:04 +0900

    Hibernate Mapping

---
+++ Source-1. ORM and JPA/BillingSystem/src/main/java/com/urunov/OrmApplication.java
@@ -0,0 +1,13 @@
+package com.urunov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmApplication.class, args);
+    }
+
+}
